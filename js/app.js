ce276482c0208e9c8f1b622f7ef6056b3c25680f
giordifungula <giordifungula@gmail.com>
2018-11-07 09:25:29 +0200

    completed check function

--- js/app.js
+++ js/app.js
@@ -21,6 +21,42 @@ let name = 'Bingo';
 function checkGuess() {
     // stores the inputed value from the user as number
     let userGuess = Number(guessField.value);
-
+    if (guessCount === 1) {
+      // this is true
+      guesses.textContent = 'Previous guesses: ';
+      // display the previous gueses
+    }
+    guesses.textContent += userGuess + ' ';
+    if (userGuess === randomNumber) {
+      // if the guese is equal to random number
+      lastResult.textContent = 'Congratulations! You got it right!';
+      lastResult.style.backgroundColor = 'green';
+      lowOrHi.textContent = '';
+      setGameOver();
+    } else if (guessCount === 10) {
+      // if the number of counts are 10 game will be over
+      lastResult.textContent = '!!!GAME OVER!!!';
+      setGameOver();
+    } else {
+      // if its not the random number and player did not reach 10
+      lastResult.textContent = 'Wrong!';
+      // player is in gameplay but got it wrong
+      lastResult.style.backgroundColor = 'red';
+      // change color to red 
+      if(userGuess < randomNumber) {
+        // if the number is wrong but less or more than random number
+        lowOrHi.textContent = 'Last guess was too low!';
+      } else if(userGuess > randomNumber) {
+        // not a else because it could be more than or equal to which is not what we want
+        lowOrHi.textContent = 'Last guess was too high!';
+        }
+        guessCount++;
+        // after they guese the number goes up by 1
+        guessField.value = '';
+        // after the number of gueses has gone up clear the guesed number
+        guessField.focus();
+        // focuses to the input text field
+      }
+      
   }
   checkGuess();
